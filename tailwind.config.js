71fae2766e898a0aa4c14e8a482620c2b2fe6565
Kothapalli Somasekhar <somunani2413@gmail.com>
2022-04-17 14:24:34 +0530

    Configured TailwindCSS v3.0.24 with plugins along side postcss v8.4.5, autoprefixer v10.4.1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
+};
